767c6623a2d99977ead22b162c13a52f00f2fbe8
TheRaginSteak <35852726+TheRaginSteak@users.noreply.github.com>
2018-01-29 20:01:52 +0000

    Add files via upload

--- garlicoin_balance_finder.py
+++ garlicoin_balance_finder.py
@@ -4,7 +4,6 @@ Need to create an external file to store addresses rather than store them as a c
 """
 import urllib.request
 import time
-import os
 
 USER_AGENT = \
     "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
@@ -18,7 +17,7 @@ def get_bool(prompt):
         except KeyError:
             print("Invalid input")
 
-def address_to_dict():
+def url_dict():
     """Converts a file with addresses in into a dictionary"""
     address_dict = {}
     with open("addresses.txt", "r") as address_file:
@@ -26,19 +25,8 @@ def address_to_dict():
             if line[:1] == "#":
                 continue
             (key, value) = line.split()
-            address_dict[key] = value
+            address_dict[key] = "https://explorer.grlc-bakery.fun/ext/getbalance/" + value
     return address_dict
-
-def address_to_url(address):
-    """Turns the address into a usable URL"""
-    return "https://explorer.grlc-bakery.fun/ext/getbalance/" + address
-
-def url_dict():
-    """Creates a dictionary with all the URLs for the wallet balances"""
-    url_dictionary = {}
-    for key, value in address_to_dict().items():
-        url_dictionary.update({key : address_to_url(value)})
-    return url_dictionary
 
 def url_value_finder(url):
     """Finds and returns the value of an address"""
@@ -51,70 +39,77 @@ def value_dict():
     value_dictionary = {}
     for key, value in url_dict().items():
         try:
-            test = value_dictionary[key]
-        except KeyError:
-            try:
+            if key in value_dictionary:
+                value_dictionary[key] += float(url_value_finder(value))
+            else:
                 value_dictionary[key] = float(url_value_finder(value))
-            except ValueError:
-                value_dictionary[key] = 0.0
-        else:
-            value_dictionary[key] += float(url_value_finder(value))
-
+        except ValueError:
+            value_dictionary[key] = 0.0
     return value_dictionary
 
-def return_total():
-    """Prints the total of all the wallet balances"""
-    total = 0
-    for i in url_dict():
-        try:
-            total += float(url_value_finder(url_dict()[i]))
-        except ValueError:
-            continue
-    return total
+def record_balance(name, balance):
+    """Records the balance into a .txt file for grlc/hr functionality"""
+    person_value = name + " " + str(balance)
+    with open(time.time() +" GRLC balances.txt", 'a') as time_file:
+        time_file.write(person_value)
+    return 1
 
-def percentage_finder_network(balance):
-    """Returns the percentage of the total supply of a wallet"""
-    total_supply = float(url_value_finder("https://explorer.grlc-bakery.fun/ext/getmoneysupply"))
-    return (balance / total_supply) * 100
+def print_values():
+    """Prints all the values for balances and Percentages"""
+    names = []
+    balances = []
+    percentages = []
+    for key, value in VALUE_DICTIONARY.items():
+        names.append(key)
+        balances.append(value)
+    for key, value in PERCENT_DICTIONARY_US.items():
+        percentages.append(value)
+    if PERCENT is True:
+        percentages_network = []
+        for key, value in percent_dict_network().items():
+            percentages_network.append(value)
 
-def percentage_of_us(balance):
-    """returns the percentage of the total out of our wallets"""
-    try:
-        return float(balance / return_total()) * 100
-    except ZeroDivisionError:
-        return 0
+    for i in enumerate(names):
+        i = i[0]
+        print(names[i].capitalize() + " " + str(round(balances[i], 3)) +
+              " Percentage of our supply: " + str(round(percentages[i], 3)) + "%")
+
+        if PERCENT is True:
+            print("Percentage of total supply: " + str(round(percentages_network[i], 5)) + "%\n")
+        else:
+            print()
+    print("Total garlic supply is: " + str(round(float(
+        url_value_finder("https://explorer.grlc-bakery.fun/ext/getmoneysupply")), 3)))
+
+def percent_dict_us():
+    """Creates a dictionary with the percentages of our supply"""
+    percent_dictionary = {}
+    total_value = 0
+    for key, value in VALUE_DICTIONARY.items():
+        total_value += value
+    for key, value in VALUE_DICTIONARY.items():
+        percent_dictionary[key] = value / total_value * 100
+    return percent_dictionary
+
+def percent_dict_network():
+    """Creates a dictionary with the percentages of the network"""
+    percent_dictionary = {}
+    total_value = url_value_finder("https://explorer.grlc-bakery.fun/ext/getmoneysupply")
+    for key, value in VALUE_DICTIONARY.items():
+        percent_dictionary[key] = value / float(total_value) * 100
+    return percent_dictionary
+
+VALUE_DICTIONARY = value_dict()
+
+PERCENT_DICTIONARY_US = percent_dict_us()
 
 PERCENT = get_bool("Do you want to view your wallet's percentage of the network? (Y/N) ")
 
-#def last_balance():
-
-
+RECORD_TIME_FILE = get_bool("Do you want to write to a file for calculating GRLC/hr later? ")
 
 def main():
     """Running the functions"""
-    print("Individual Balances:\n")
-    total = 0
-    value_dictionary = value_dict()
-    with open(time.strftime("%d.%m.%Y %H%M GRLC balances.txt"), 'a') as time_file:
-        if PERCENT is True:
-            for key, value in value_dictionary.items():
-                person_value = key.capitalize() + " " + str(round(value, 3))
-                time_file.write(person_value)
-                print(person_value +
-                      " Percentage of our supply: " + str(round(percentage_of_us(value))) +
-                      "% Percentage of total supply: " +
-                      str(round(percentage_finder_network(value), 3)) + "%\n")
-                total += value
-
-        else:
-            for key, value in value_dictionary.items():
-                person_value = key.capitalize() + " " + str(round(value, 3)) + "\n"
-                print(person_value)
-                time_file.write(person_value)
-                total += value
-
-    print("Total balance is: " + str(round(total, 4)) + " GRLC")
-    print("Total garlic supply is: " + str(round(float(url_value_finder("https://explorer.grlc-bakery.fun/ext/getmoneysupply")), 4)) + " GRLC")
+    print_values()
 
 if __name__ == "__main__":
     main()
