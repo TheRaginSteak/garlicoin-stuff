1d20e6c037b75565c1f4c29f59a2b07b8fda2530
unknown <harry.werring@gmail.com>
2018-02-04 20:58:11 +0000

    Properly done garlicoin_grapher.py because Rubin is incompetant

--- garlicoin_grapher.py
+++ garlicoin_grapher.py
@@ -1,13 +1,39 @@
-"""Module to create graphs from garlicoin balances
-WIP
+"""
+Module to create graphs from garlicoin balances
 """
 import matplotlib.pyplot as plt
-from garlicoin_balance_finder import value_dict
+from garlicoin_balance_finder import value_dict, get_bool
 
-def plot_bar_graph():
-    plt.ylabel('GRLC')
-    names = []
-    values = []
-    for key, value in value_dict():
-        names.append([key.capitalize()])
-        values.append(value)
+CHART = get_bool("Do you want a pie chart? (Y/N) ")
+
+def pie_chart():
+    labels = []
+    sizes = []
+    explode = []
+    colors = []
+    USABLE_COLOURS = ["red", "orange", "yellow", "green", "blue", "indigo", "lightgreen", "lightblue", "gold"]
+    for key,value in value_dict().items():
+        if value > 0:
+          labels.append(key.capitalize())
+          sizes.append(value)
+    for size in sizes:
+        if size < 10:
+            explode.append(0.4)
+        else:
+            explode.append(0)
+    for i in range(len(sizes)):
+        colors.append(USABLE_COLOURS[i])
+
+    plt.pie(sizes, explode = explode, labels = labels, colors = colors, autopct = make_autopct(sizes))
+    plt.axis("equal")
+    plt.show()
+
+def make_autopct(sizes):
+    def my_autopct(pct):
+        total = sum(sizes)
+        val = int(round(pct * total / 100))
+        return("{p:.2f}% ({v:d})".format(p = pct, v = val))
+    return my_autopct
+
+if __name__ == '__main__':
+    pie_chart()
