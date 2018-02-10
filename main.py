a8087fdbe3299d70f823262966b4d051e2609e08
Nobody0201 <35852996+Nobody0201@users.noreply.github.com>
2018-02-10 21:04:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 Runs all the modules
 """
 
-import garlicoin_balance_finder as grlc_balance
-import garlicoin_grapher as grlc_grapher
+import checker as grlc_balance
+import chart as grlc_grapher
+import line_graph as grlc_line
 
 PRINTING_VALUES = grlc_balance.get_bool("Do you want to print garlicoin values? (Y/N) ")
 
@@ -12,8 +13,12 @@ def main():
         grlc_balance.main()
 
     CHART = grlc_balance.get_bool("Do you want a chart? (Y/N) ")
-    if CHART is True:
+    if CHART:
         grlc_grapher.main()
+    
+    LINE = grlc_balance.get_bool("Do you want a line graph? (Y/N) ")
+    if LINE:
+        grlc_line.main()
 
 if __name__ == '__main__':
     main()
