c795d10f97be602e9354e2dc394b1bbe44ccced4
TheRaginSteak <harry.werring@gmail.com>
2018-02-10 20:51:12 +0000

    Stopped pylint throwing a fit

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ Runs all the modules
 import garlicoin_balance_finder as grlc_balance
 import garlicoin_grapher as grlc_grapher
 
-PRINTING_VALUES = grlc_balance.get_bool("Do you want to print garlicoin values? (Y/N) ")
-
 def main():
-    if PRINTING_VALUES is True:
+    """Runs the modules the user wants"""
+    printing_values = grlc_balance.get_bool("Do you want to print garlicoin values? (Y/N) ")
+    if printing_values is True:
         grlc_balance.main()
 
-    CHART = grlc_balance.get_bool("Do you want a chart? (Y/N) ")
-    if CHART is True:
+    chart = grlc_balance.get_bool("Do you want a chart? (Y/N) ")
+    if chart is True:
         grlc_grapher.main()
 
 if __name__ == '__main__':
