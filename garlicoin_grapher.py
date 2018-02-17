d7b0563bfc9dffac907751f8782438ba10d420c2
TheRaginSteak <harry.werring@gmail.com>
2018-02-17 19:59:16 +0000

    added rubin's latest code (not tested whatsoever)

--- garlicoin_grapher.py
+++ garlicoin_grapher.py
@@ -71,13 +71,12 @@ def line_graph():
         for i in data:
             big_list.append(i.split("\n"))
         big_list[-1].remove("")
+        names = [str(i.split()[0]) for i in big_list[0] if i.split()[0].isalpha()]
         datetimes = [datetime.datetime.fromtimestamp(float(i[0])) for i in big_list]
-        num = 1
-        for i in big_list:
-            values = [i[num].split()[1] for i in big_list]
-            plt.plot(datetimes, values, linewidth=2.0, label=i[num].split()[0])
-            num += 1
-            print(i[num].split()[0])
+        raw_values = [[float(item[num].split()[1]) for num in range(1, len(big_list[0]))] for item in big_list]
+        values = list(zip(*raw_values))
+        for i in range(len(values)):
+            plt.plot(datetimes, values[i], label=names[i])
         plt.ylabel("garlicoin")
         plt.xlabel("time")
         plt.legend()
